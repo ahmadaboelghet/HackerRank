fba7a950cdd7b2b34b1e214cbbce355c5cfd2e61
aBoelghet <aboelghet.ios.dev@gmail.com>
2023-09-11 08:27:25 +0300

    findMedian PS

--- HackerRank/main.swift
+++ HackerRank/main.swift
@@ -55,56 +55,78 @@ import Foundation
 //}
 //
 //miniMaxSum(arr: [1,3,5,7,9])
+//
+//
+//
+//func timeConversion(s: String) -> String {
+//
+//    let dateString = s
+//
+//    var hour = 0
+//    var minute = 0
+//    var second = 0
+//    var hourString = ""
+//    var minuteString = ""
+//    var secondString = ""
+//
+//    // Create a date formatter
+//    let dateFormatter = DateFormatter()
+//    dateFormatter.dateFormat = "hh:mm:ssa" // This format matches the input string
+//
+//    // Parse the string into a Date object
+//    if let date = dateFormatter.date(from: dateString) {
+//        // Create a calendar instance to extract date components
+//        let calendar = Calendar.current
+//
+//        // Extract date components
+//        let components = calendar.dateComponents([.hour, .minute, .second], from: date)
+//
+//        // Access the individual components
+//        hour = components.hour ?? 0
+//
+//        minute = components.minute ?? 0
+//        second = components.second ?? 0
+//        if hour < 10 {
+//            hourString = "0" + "\(hour)"
+//        }else{
+//            hourString = "\(hour)"
+//        }
+//        if minute < 10 {
+//            minuteString = "0" + "\(minute)"
+//        }else{
+//            minuteString =  "\(minute)"
+//        }
+//        if second < 10 {
+//            secondString = "0" + "\(second)"
+//        }else{
+//            secondString = "\(second)"
+//        }
+//        return hourString + ":" + minuteString + ":" + secondString
+//    } else {
+//        return "Invalid date string"
+//    }
+//
+//}
+//print(timeConversion(s: "07:05:45PM"))
 
 
-
-func timeConversion(s: String) -> String {
-
-    let dateString = s
-
-    var hour = 0
-    var minute = 0
-    var second = 0
-    var hourString = ""
-    var minuteString = ""
-    var secondString = ""
+func findMedian(arr: [Int]) -> Int {
     
-    // Create a date formatter
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "hh:mm:ssa" // This format matches the input string
-
-    // Parse the string into a Date object
-    if let date = dateFormatter.date(from: dateString) {
-        // Create a calendar instance to extract date components
-        let calendar = Calendar.current
-
-        // Extract date components
-        let components = calendar.dateComponents([.hour, .minute, .second], from: date)
-
-        // Access the individual components
-        hour = components.hour ?? 0
-         
-        minute = components.minute ?? 0
-        second = components.second ?? 0
-        if hour < 10 {
-            hourString = "0" + "\(hour)"
-        }else{
-            hourString = "\(hour)"
-        }
-        if minute < 10 {
-            minuteString = "0" + "\(minute)"
-        }else{
-            minuteString =  "\(minute)"
-        }
-        if second < 10 {
-            secondString = "0" + "\(second)"
-        }else{
-            secondString = "\(second)"
-        }
-        return hourString + ":" + minuteString + ":" + secondString
-    } else {
-        return "Invalid date string"
+    var sortedArr = arr.sorted()
+    let count = arr.count
+    guard count > 0 else {
+        return 0 // Return nil for an empty array
     }
-
+    if count % 2 == 1 {
+        // Odd number of elements, return the middle element
+        return sortedArr[count / 2]
+        
+    } else {
+        // Even number of elements, return the average of the two middle elements
+        let middle1 = sortedArr[count / 2 - 1]
+        let middle2 = sortedArr[count / 2]
+        return (middle1 + middle2) / 2
+        
+    }
 }
-print(timeConversion(s: "07:05:45PM"))
+print(findMedian(arr: [1,2,3,4,5,6]))
